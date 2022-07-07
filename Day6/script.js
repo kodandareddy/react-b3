1fc5e3833233b40a0e23f9f37bba0dd294cb6758
kodanda reddy <kodandareddy21@gmail.com>
2022-07-07 21:08:12 +0530

    Js intro

---
+++ Day6/script.js
@@ -0,0 +1,11 @@
+// var data="React"
+
+// var data1=true
+
+// var myNumber="1"
+
+
+var myVar_1 = String(45);	
+
+console.log(typeof myVar_1)
+// https://www.scaler.com/topics/javascript/type-coercion-javascript/
