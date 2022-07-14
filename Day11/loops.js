5afe59a01c4c60865c4996a1bf4fac328bd7d54e
kodanda reddy <kodandareddy21@gmail.com>
2022-07-14 21:18:23 +0530

    Day11

---
+++ Day11/loops.js
@@ -0,0 +1,31 @@
+var myNumbers=[2,4,5,6,7,8]
+// for(intitialization;condition;increment){
+
+// }
+
+// for(var i=0;i<myStates.length;i=i+1){
+//     console.log(myStates[i])
+// }
+
+
+// var myOutput=[]
+// for(var i=0;i<myNumbers.length;i=i+1){
+//     myOutput.push(myNumbers[i]%2)
+// }
+
+// console.log(myOutput)
+
+https://www.programiz.com/javascript/for-loop
+
+// program to display the sum of natural numbers
+// let n = 100
+// let sum = 0;
+
+// // looping from i = 1 to n
+// // in each iteration, i is increased by 1
+// for (let i = 1; i <= n; i++) {
+   
+//     sum += i;  // sum = sum + i
+// }
+
+// console.log('sum:', sum);
--- Day11/objects.js
+++ Day11/objects.js
@@ -1,11 +1,26 @@
-var user={
-    firstName:"ram",
-    lastName:"Reddy",
-    role:"admin",
-    loginCount:20,
-    faceBookSignin:true
-}
+console.log(this);
 
-console.log(user.ram) 
-user.lastName
-user.role
+var user = {
+  firstName: "ram",
+  lastName: "Reddy",
+  role: "admin",
+  loginCount: 20,
+  courseName: ["React"],
+  faceBookSignin: true,
+  buyCourse:function  () {
+    console.log(this,"++++")
+    // this.courseName.push("Node js");
+  },
+  loginUser: function () {
+    this.loginCount = this.loginCount + 1;
+  },
+  changeMylogin: function () {
+    this.faceBookSignin = false;
+  },
+  changeMyName: function (name) {
+    this.firstName=name
+  },
+};
+
+user.changeMyName("Rama Reddy");
+console.log(user.buyCourse())
